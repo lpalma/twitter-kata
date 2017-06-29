cd05e01ede6fb7d276a85fc194d24ee3f7c8479f
Luciano Palma <lucianopalma90@gmail.com>
2017-06-29 17:27:11 +0100

    create InputLine VO

---
+++ src/main/java/com/codurance/twitterKata/valueObject/InputLine.java
@@ -0,0 +1,13 @@
+package com.codurance.twitterKata.valueObject;
+
+public class InputLine {
+    private String input;
+
+    public InputLine(String input) {
+        this.input = input;
+    }
+
+    public String input() {
+        return input;
+    }
+}
